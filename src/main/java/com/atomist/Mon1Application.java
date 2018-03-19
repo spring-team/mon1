db9330a0e1e4b99e96e2d6a3f2e87360d20a9d03
Rod Johnson <johnsonroda@gmail.com>
2018-03-19 19:42:40 +1100

    Everyone needs encouragement to write Java

--- src/main/java/com/atomist/Mon1Application.java
+++ src/main/java/com/atomist/Mon1Application.java
@@ -1,3 +1,5 @@
+// I come from the loving space of my heart, and I know that love opens all doors.
+
 // As I say yes to life, life says yes to me.
 
 // You are the delta in what you do, not the things you did in the past
