62782663314e457237ff65b0d4abeeae7fe8cdda
Rod Johnson <johnsonroda@gmail.com>
2018-03-19 18:10:30 +1100

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/Mon1Application.java
@@ -0,0 +1,12 @@
+package com.atomist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mon1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mon1Application.class, args);
+	}
+}
